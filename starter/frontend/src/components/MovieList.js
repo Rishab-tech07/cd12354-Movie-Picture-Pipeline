19ca3a135068f8adab912c9ea5eaf8c10288c325
Dhruv Kumar <dhruvkumar9173@gmail.com>
2026-09-19 10:24:53 +0530

    Normalize frontend backend API URL

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,11 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+    const apiBaseUrl = process.env.REACT_APP_MOVIE_API_URL
+      .replace(/\/+$/, '')
+      .replace(/\/movies$/, '');
+
+    axios.get(`${apiBaseUrl}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
   }, []);
